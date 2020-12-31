d661b05685bfeb2dea1f4d7e50cbfae15974c468
PyFinn <realstraved@gmail.com>
2020-12-31 16:28:03 +0100

    Grid setup

--- lib/categories.dart
+++ lib/categories.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
+import './dummy_data.dart';
+import './category_item.dart';
 
 class Categories extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: [],
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Delicious Meals'),
+      ),
+      body: GridView(
+        children: DUMMY_CATEGORIES
+            .map((catData) => CategoryItem(catData.title, catData.color))
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+      ),
     );
   }
 }
