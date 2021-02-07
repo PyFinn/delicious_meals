e432ba66f504e3943664c1d92498e3b3d04f5ccd
PyFinn <realstraved@gmail.com>
2021-02-07 20:44:25 +0100

    Bottom Navigation

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -8,34 +8,41 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Widget> pages = [Categories(), FavoritesScreen()];
+
+  int _selectedPage = 0;
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text('Meals'),
-            bottom: TabBar(
-              tabs: [
-                Tab(
-                  icon: Icon(
-                    Icons.category,
-                  ),
-                  text: 'Categories',
-                ),
-                Tab(
-                  icon: Icon(Icons.star),
-                  text: 'Favorites',
-                )
-              ],
-            ),
-          ),
-          body: TabBarView(
-            children: [
-              Categories(),
-              FavoritesScreen()
-            ],
-          ),
-        ));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meals'),
+      ),
+      drawer: Drawer(
+        child: Text('The Drawer!'),
+      ),
+      body: pages[_selectedPage],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (int index) {
+          setState(() {
+            _selectedPage = index;
+          });
+        },
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        type: BottomNavigationBarType.shifting,
+        currentIndex: _selectedPage,
+        items: [
+          BottomNavigationBarItem(
+              backgroundColor: Theme.of(context).primaryColor,
+              icon: Icon(Icons.category),
+              label: 'Categories'),
+          BottomNavigationBarItem(
+              backgroundColor: Theme.of(context).primaryColor,
+              icon: Icon(Icons.star),
+              label: 'Favorites')
+        ],
+      ),
+    );
   }
 }
